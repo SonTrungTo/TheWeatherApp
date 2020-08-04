f806b6e9319026523f678afd0892ea1de6bb26e0
SonTrungTo <son.trung.to@gmail.com>
2020-08-04 03:00:34 +0300

    Weather App

---
+++ README.md
@@ -0,0 +1,2 @@
+# Weather App
+To show information about current weather.
---
+++ public/main.js
@@ -0,0 +1,14 @@
+// let form = document.querySelector("form");
+// let header = document.querySelector("h1");
+//
+// form.addEventListener("submit", event => {
+//   event.preventDefault();
+//
+//   let city = event.target.elements.city.value.trim();
+//
+//   fetch("/", {header: {"Content-Type": "application/json"}}).then(res => res.text())
+//   .then(res => {
+//
+//     header.textContent = `${res}`;
+//   });
+// });
